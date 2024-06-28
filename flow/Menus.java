1fc967bf31001f0aea42c2250daf4a695e4634eb
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-06-28 13:14:15 +0300

    Methods 2

--- flow/Menus.java
+++ flow/Menus.java
@@ -3,57 +3,63 @@ package flow;
 import java.util.Scanner;
 
 public class Menus {
-    public static void main(String[] args) {
-        System.out.println("MENUS");
+    // Class / Local Variable
+    static Scanner kb = new Scanner(System.in);
+    static String choice = "";
 
-        Scanner kb = new Scanner(System.in);
-        String choice = "";
+    public static void main(String[] args) {
+        System.out.println("MENUS");        
 
         do {
-            System.out.println("---------------");
-            System.out.println("MAIN MENU");
-            System.out.println("1. Intro");
-            System.out.println("2. Basic");
-            System.out.println("3. Advanced");
-            System.out.println("4. Expert");
-            System.out.println("X. Exit");
-            System.out.print("Select: ");
-            choice = (kb.nextLine()).trim().toUpperCase();
-            System.out.println("---------------");
-
+            printMenus();
+            
             switch (choice) {
                 case "1":
                     System.out.println("INTRO");
-                    System.out.println("Press any key to continue...");
-                    kb.nextLine();
+                    printPressAnyKey();
                     break;
                 case "2":
                     System.out.println("BASIC");
-                    System.out.println("Press any key to continue...");
-                    kb.nextLine();
+                    printPressAnyKey();
                     break;
                 case "3":
                     System.out.println("ADVANCED");
-                    System.out.println("Press any key to continue...");
-                    kb.nextLine();
+                    printPressAnyKey();
                     break;
                 case "4":
                     System.out.println("EXPERT");
-                    System.out.println("Press any key to continue...");
-                    kb.nextLine();
+                    printPressAnyKey();
                     break;
                 case "X":
                     System.out.println("Bye!");
                     break;
                 default: // Catch All
                     System.out.println("Invalid Choice");
-                    System.out.println("Press any key to continue...");
-                    kb.nextLine();
+                    printPressAnyKey();
             }
         } while (!choice.equals("X"));
         // choice != "X"
         // choice == "X"
 
         kb.close();
+    }
+
+
+    static void printMenus() {
+        System.out.println("---------------");
+        System.out.println("MAIN MENU");
+        System.out.println("1. Intro");
+        System.out.println("2. Basic");
+        System.out.println("3. Advanced");
+        System.out.println("4. Expert");
+        System.out.println("X. Exit");
+        System.out.print("Select: ");
+        choice = (kb.nextLine()).trim().toUpperCase();
+        System.out.println("---------------");
+    }
+
+    static void printPressAnyKey() {
+        System.out.println("Press any key to continue...");
+        kb.nextLine();
     }
 }

c500bfa0372098f1df107672d287f2cfae157bc6
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-06-28 11:23:30 +0300

    While

--- flow/Loops.java
+++ flow/Loops.java
@@ -11,9 +11,19 @@ public class Loops {
         System.out.println(a);
 
         // do-while Statement
+        // Guaranteed 1 pass
         int i = 10;
         do {
-            System.out.println(++i); // 10\n
+            System.out.println("i " + ++i); // 10\n
         } while (i <= 10); // Forever Loop
+
+
+        // while Statement
+        int j = 11;
+        while (j <= 10) {
+            System.out.println("j " + j++);
+        }
+
+        
     }
 }

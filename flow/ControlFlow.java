48d268f92dc5bb97cc5f90930ce10a4de0cad359
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-06-28 10:40:57 +0300

    Ifs

---
+++ flow/ControlFlow.java
@@ -0,0 +1,45 @@
+package flow;
+
+public class ControlFlow {
+    public static void main(String[] args) {
+        // CONTROL FLOW STATEMENTS
+
+        // if Statement
+        boolean a = true;
+
+        if (!a) { // ! (not)
+            System.out.println("Evaluated to true");
+        }
+
+
+        // if-else Statemnent
+        char c = 'A';
+
+        if (c == 'X') {
+            System.out.println("Exit");
+        } else {
+            System.out.println("Continue");
+        }
+
+
+        // if-else-if Statement
+        int i = 1;
+        long l = 12323264644545L;
+        double d = 1.33333333;
+
+        float n = 80.00F;
+        float m = (float) 10 / 100; // 10% = 0.100
+
+        float f = 75.50F; // 75.5 + 7.55 = 83.05
+        
+        if (f + (f * m) < n) {
+            System.out.println("Below Normal");
+        } else if (f + (f * m) == n) {
+            System.out.println("Normal");
+        } else if (f + (f * m) == n) {
+            System.out.println("Above Normal");
+        } else {
+            System.out.println("Invalid Result");
+        }
+    }
+}

2b902ae555f38affca33baa53d7e662ff6b0c034
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-07-05 13:21:51 +0300

    Password Generator

---
+++ arrays/PasswordGenerator.java
@@ -0,0 +1,24 @@
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        System.out.println("PASSWORD GENERATOR");
+
+        // Password Format: Abc_123
+        // 1 uppercase letter
+        // 1 or more lowercase letters (N times)
+        // 1 symbol
+        // 1 or more numbers (M times)
+
+        // Letters: A-Z a-z
+        // Numbers: 0-9
+        // Symbols: _ # @ .
+
+        int N = 2; // 2 small letters
+        int M = 3; // 3 digits
+
+        // HINT
+        // 1 Random
+        // N loop
+        // 1 Random
+        // M loop
+    }
+}

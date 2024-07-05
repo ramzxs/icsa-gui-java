6056e67b8a49d92ebaa52e723b6adeaa37821aef
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-07-05 12:52:14 +0300

    CAPTCHA

---
+++ arrays/Captcha.java
@@ -0,0 +1,21 @@
+public class Captcha {
+    public static void main(String[] args) {
+        System.out.println("CAPTCHA");
+
+        char[] symbols = {
+            'A', 'B', 'C',
+            'a', 'b', 'c',
+            '1', '2', '3',
+            '_', '@', '.'
+        };
+
+        int N = 3;
+
+        // ALGO
+        // Set result = "";
+        // Loop N times
+        //   Generate random number between 0 to symbols.length - 1
+        //   Save the character to result
+        // Print result
+    }
+}
